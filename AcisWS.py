65e4a76496829bac55be9decbb56decbab87cac0
Britta Daudert <bdaudert@dri.edu>
2012-11-17 12:13:39 -0800

    state averages highchart graph

--- AcisWS.py
+++ AcisWS.py
@@ -10,6 +10,7 @@ sodlist, sodsum
 
 #############################################################################
 #python modules
+import numpy
 import datetime
 import sys
 from collections import defaultdict
@@ -97,45 +98,54 @@ def get_station_meta(by_type, val):
     f.close()
     return stn_json
 
-def mean_temp_prcp(state, end_date):
-    year = end_date[0:4]
-    mon = end_date[4:6]
-    day = end_date[6:8]
-    start_date = "1900%s%s" %( mon, day)
-    num_yrs = int(year) - 1900 - 1
-    state_ave_temp = [999 for yr in range(num_yrs)]
-    state_ave_pcpn = [999 for yr in range(num_yrs)]
-    params = {"state":state,"sdate":start_date,"edate":end_date, \
-    "elems":[{"name":"avgt","interval":[1,0,0],"duration":30,"reduce":"mean","smry":"mean"}, \
-    {"name":"avgt","interval":[1,0,0],"duration":30,"reduce":"mean","smry":"mean"}]}
+#Rountine computes mean precip and temp for month given in end_date
+#for each year starting 1900 ending end_date[year] - 1
+#def mean_temp_pcpn(state, month, elements):
+def state_aves(state, month, elements):
+    mon = int(month)
+    if mon not in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]:
+        print "Not a valid month: %s. Enter a number 1-12" % str(mon)
+        sys.exit(0)
+
+    mon_lens = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    today = datetime.date.today()
+    year = today.year
+    start_year = 1900
+    year_list = []
+    if WRCCUtils.is_leap_year(year) and mon == 2:
+        start_date = "%s%s%s" %(str(start_year), str(mon), str(29))
+        end_date = "%s%s%s" % (str(year), str(mon), str(29))
+    else:
+        end_date = "%s%s%s" % (str(year), str(mon), str(mon_lens[mon - 1]))
+        start_date = "%s%s%s" %(str(start_year), str(mon), str(mon_lens[mon - 1]))
+
+    el_list = [{"name":el, "interval":[1,0,0],"duration":30,"reduce":"mean","smry":"mean"} for el in elements]
+    params = {"state":state,"sdate":start_date,"edate":end_date, "elems":el_list}
     request = MultiStnData(params)
+    state_aves = defaultdict(dict)
     if 'error' in request:
-        state_ave_temp = request['error']
-        state_ave_pcpn = request['error']
+        state_ave_temp = []
+        state_ave_pcpn = []
     try:
-        for yr in num_years:
-            ave_temp_list = []
-            ave_pcp_list = []
+        for k, el in enumerate(elements):
+            state_aves[k] = {'element': el, 'state_ave': [999 for yr in range(len(request['data'][0]['data']))]}
+        state_ave_temp = [999 for yr in range(len(request['data'][0]['data']))]
+        state_ave_pcpn = [999 for yr in range(len(request['data'][0]['data']))]
+    except:
+        state_ave_temp = []
+        state_ave_pcpn = []
+
+    for yr in range(len(request['data'][0]['data'])):
+        year_list.append("%s" % str(start_year + yr))
+        for k,el in enumerate(elements):
+            ave_list = []
             for stn in request['data']:
                 try:
-                    ave_temp_list.append(float(stn['data'][yr][0]))
+                    ave_list.append(float(stn['data'][yr][k]))
                 except:
                     pass
-                try:
-                    ave_pcpn_list.append(float(stn['data'][yr][1]))
-                except:
-                    pass
-
-            if ave_temp_list:
-                state_ave_temp[yr] = numpy.mean(ave_temp_list)
-
-            if ave_pcpn_list:
-                state_ave_pcpn = numpy.mean(ave_pcpn_list)
-
-    except:
-        pass
-
-    return state_ave_temp, state_ave_pcpn
+            state_aves[k]['state_ave'][yr] = numpy.mean(ave_list)
+    return year_list, state_aves
 
 def get_point_data(form_input, program):
     def get_stn_list(by_type, val):
